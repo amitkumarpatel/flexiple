4afa226bbdad29d9fc87f949e8fcdbc5fe14e88a
amitkumarpatel <amitkumarpatel44@gmail.com>
2016-12-16 20:21:35 +0530

    updated in user file for email content

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,20 +11,21 @@ class User < ActiveRecord::Base
   def email_constraint
     domain = email.split("@").last
     if !email.blank?
-      errors.add(:email, "Invalid Domain") unless /\.co$/  === domain
+      errors.add(:email, "Invalid domain must contain .in") unless /\.in$/  === domain
     end
   end
     
   def name_constraint
     if !name.blank?
-      errors.add(:name, "Name not allowed") unless /^flexiple_[a-z_]*$/  === name
-    end  	
+      errors.add(:name, " not allowed must prefix flexiple_") unless /^flexiple_[a-z_]*$/  === name
+    end   
   end
 
   def password_constraint
     if !password.blank?
-      errors.add(:password, "Password not allowed") unless (/[A-Z]{2}+/ === password  && /[*@!#%&()^~{}]{2}+/ === password)
-    end  	
+      errors.add(:password, " not allowed, 2 capital and 2 special character required") unless (/[A-Z]{2}+/ ===
+          password  && /[*@!#%&()^~{}]{2}+/ === password)
+    end   
   end
 
 end
